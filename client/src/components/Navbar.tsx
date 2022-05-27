48f2af6ffacdabc6ea088a89f9c05237859a4b6e
Matthew Brignola <matthewbrignola@ymail.com>
2022-05-27 14:41:27 -0600

    mobile optimized navbar with hamburger menu

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,56 @@
+import { useEffect, useState } from "react";
 import img from "../img/prismatic-todos.png";
+import { FaBars } from "react-icons/fa";
 function Navbar() {
-  return (
+  const [mobile, setMobile] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [hamburger, setHamburger] = useState(false);
+  function checkWindow() {
+    if (window.innerWidth < 900) {
+      setMobile(true);
+      return;
+    }
+  }
+  const resizeWindow = () => {
+    setWidth(window.innerWidth);
+    width < 700 ? setMobile(true) : setMobile(false);
+    console.log(width, mobile);
+  };
+  useEffect(() => {
+    checkWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  });
+  return mobile ? (
+    <div className="mobile-menu">
+      <a href="/">
+        <img
+          src={img}
+          style={{ height: 75, margin: -30, padding: 10 }}
+          alt="Prismatic Todos"
+        />
+      </a>
+      <FaBars
+        style={{ color: "#4504ea", float: "right" }}
+        onClick={() => setHamburger(!hamburger)}
+      />
+      {hamburger ? (
+        <ul className="hamburger">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/todos">Todos</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      ) : (
+        <></>
+      )}
+    </div>
+  ) : (
     <nav>
       <ul>
         <li className="img" style={{ marginRight: "2em" }}>
--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,45 +1,102 @@
+import { useEffect, useState } from "react";
 import { FaGithub, FaEnvelope, FaSlack } from "react-icons/fa";
 // import Email from "../components/Email";
 const Contact = () => {
+  const [mobile, setMobile] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const resizeWindow = () => {
+    setWidth(window.innerWidth);
+    width < 800 ? setMobile(true) : setMobile(false);
+    console.log(width, mobile);
+  };
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  });
   return (
     <>
       <section className="page">
         <div className="title">Contact</div>
-        <article className="paragraph">
-          <section className="contact-card">
-            <a
-              href="mailto:matthewbrignola@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-              style={{ textAlign: "center" }}
-            >
-              <FaEnvelope className="icon" style={{ fontSize: "3em" }} />
-              <h3>matthewbrignola@gmail.com</h3>
-            </a>
-          </section>
-          <section className="contact-card">
-            <a
-              href="http://github.com/prismaticdevs"
-              target="_blank"
-              rel="noreferrer"
-              style={{ textAlign: "center" }}
-            >
-              <FaGithub className="icon" style={{ fontSize: "3em" }} />
-              <h3>github.com/PrismaticDevs</h3>
-            </a>
-          </section>
-          <section className="contact-card">
-            <a
-              href="https://media-gvn6693.slack.com"
-              target="_blank"
-              rel="noreferrer"
-              style={{ textAlign: "center" }}
-            >
-              <FaSlack className="icon" style={{ fontSize: "3em" }} />
-              <h3>send us a message on Slack</h3>
-            </a>
-          </section>
-        </article>
+        {mobile ? (
+          <article
+            className="paragraph"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <section className="contact-card">
+              <a
+                href="mailto:matthewbrignola@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaEnvelope className="icon" style={{ fontSize: "3em" }} />
+                <h3>matthewbrignola@gmail.com</h3>
+              </a>
+            </section>
+            <section className="contact-card">
+              <a
+                href="http://github.com/prismaticdevs"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaGithub className="icon" style={{ fontSize: "3em" }} />
+                <h3>github.com/PrismaticDevs</h3>
+              </a>
+            </section>
+            <section className="contact-card">
+              <a
+                href="https://media-gvn6693.slack.com"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaSlack className="icon" style={{ fontSize: "3em" }} />
+                <h3>send us a message on Slack</h3>
+              </a>
+            </section>
+          </article>
+        ) : (
+          <article className="paragraph">
+            <section className="contact-card">
+              <a
+                href="mailto:matthewbrignola@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaEnvelope className="icon" style={{ fontSize: "3em" }} />
+                <h3>matthewbrignola@gmail.com</h3>
+              </a>
+            </section>
+            <section className="contact-card">
+              <a
+                href="http://github.com/prismaticdevs"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaGithub className="icon" style={{ fontSize: "3em" }} />
+                <h3>github.com/PrismaticDevs</h3>
+              </a>
+            </section>
+            <section className="contact-card">
+              <a
+                href="https://media-gvn6693.slack.com"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign: "center" }}
+              >
+                <FaSlack className="icon" style={{ fontSize: "3em" }} />
+                <h3>send us a message on Slack</h3>
+              </a>
+            </section>
+          </article>
+        )}
         <br />
         {/* <Email /> */}
       </section>
